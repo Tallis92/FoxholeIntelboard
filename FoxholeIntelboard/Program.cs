cdc52a62a7bdb9f641d0451d9f45de3a9481142f
Tim Tallberg <tim.tallis@hotmail.com>
2025-05-13 13:21:00 +0200

    Added scopes for managers in program.cs

--- FoxholeIntelboard/Program.cs
+++ FoxholeIntelboard/Program.cs
@@ -1,10 +1,15 @@
 
 using Microsoft.EntityFrameworkCore;
+using FoxholeIntelboard.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<AmmunitionManager>();
+builder.Services.AddScoped<MaterialManager>();
+builder.Services.AddScoped<ResourceManager>();
+
 
 var app = builder.Build();
 Console.WriteLine("Build is starting...");
@@ -14,7 +19,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseRouting();
@@ -24,9 +28,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
-Console.WriteLine("App is starting...");
+
 app.Run();
 
-
-// Configure the HTTP request pipeline.
 
