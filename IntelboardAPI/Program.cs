aa1c46c70abf2a32b69f5d6f60f15a9e021e3ab0
Robin Jonsson <jonsson_robin@icloud.com>
2025-05-19 13:44:07 +0200

    Structured up API program.cs

--- IntelboardAPI/Program.cs
+++ IntelboardAPI/Program.cs
@@ -1,39 +1,13 @@
-using IntelboardAPI.Data;
-using IntelboardAPI.Services;
-using Microsoft.EntityFrameworkCore;
+using IntelboardAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy
-          .WithOrigins("https://localhost:7096")  
-          .AllowAnyHeader()                        
-          .AllowAnyMethod()                        
-          .AllowCredentials();                  
-    });
-});
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-
-builder.Services.AddDbContext<IntelboardDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("IntelboardDB")));
-
-builder.Services.AddScoped<IResourceService, ResourceService>();
-builder.Services.AddScoped<IMaterialService, MaterialService>();
-builder.Services.AddScoped<IAmmunitionService, AmmunitionService>();
-builder.Services.AddScoped<IWeaponService, WeaponService>();
+builder.AddServices();
 
 var app = builder.Build();
 
 app.UseCors("AllowFrontend");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
