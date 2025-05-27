95267422df0282acc604d7ffcea1558b2d24c05c
Tim Tallberg <tim.tallis@hotmail.com>
2025-05-27 11:28:08 +0200

    Added DemoUser to demonstrate role permissions and authorities with list management. Created Cookie to store the selecter role in the session

--- FoxholeIntelboard/Program.cs
+++ FoxholeIntelboard/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddScoped<WeaponManager>();
 builder.Services.AddScoped<CategoryManager>();
 builder.Services.AddScoped<InventoryManager>();
 builder.Services.AddScoped<CraftableItemManager>();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<IntelboardDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("IntelboardDB")));
 
