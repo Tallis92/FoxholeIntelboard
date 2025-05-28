c69d058a87e43848350806d38fddaf8c3f3da884
Robin Jonsson <jonsson_robin@icloud.com>
2025-05-28 09:57:10 +0200

    Deleted uneccessary method and refactored CSV file to avoid null reference

--- FoxholeIntelboard/Configuration/ConfigureProgram.cs
+++ FoxholeIntelboard/Configuration/ConfigureProgram.cs
@@ -17,6 +17,7 @@ namespace FoxholeIntelboard.Configuration
             builder.Services.AddScoped<WeaponManager>();
             builder.Services.AddScoped<CategoryManager>();
             builder.Services.AddScoped<InventoryManager>();
+            builder.Services.AddScoped<CraftableItemManager>();
             builder.Services.AddDbContext<IntelboardDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("IntelboardDB")));
         }
